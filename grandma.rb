751068454f6b7f2c4cd240da3e472ed29b17c6e6
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-16 09:47:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!" 
+    else if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
   end 
